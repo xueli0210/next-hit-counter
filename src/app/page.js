af7565faa3b1a904aacae4ba297e14655e86d15d
Xue Li <xuelimiis@gmail.com>
2024-09-23 19:46:55 +0000

    [feat] App: Add censored effect

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import Censored from "../components/Censored";
 import HitCounter from '../components/HitCounter'
 
 function Home() {
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number <HitCounter />.</p>
+      <p>
+        You are visitor number{" "}
+        <Censored>
+          <HitCounter />
+        </Censored>
+        .
+      </p>
     </main>
   );
 }
