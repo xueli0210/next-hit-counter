4bf5471067fbcf0e1579b53fddc860881a9dad4c
Xue Li <xuelimiis@gmail.com>
2024-09-23 19:38:47 +0000

    [prep] refactor page to create HitCounter component

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,12 @@
 import React from 'react';
 
-import {
-  readFile,
-  writeFile,
-} from '../helpers/file-helpers';
-
-const DATABASE_PATH = '/src/database.json';
-
-/*
-`readFile` takes 1 argument:
-• the path to the file:
-
-readFile('/path/to/file');
-
-`writeFile` takes 2 arguments:
-• The path to the file
-• The new contents for the file
-
-writeFile(
-  '/path/to/file',
-  '{ "hello": "world" }'
-);
-*/
+import HitCounter from '../components/HitCounter'
 
 function Home() {
-  let hits = JSON.parse(readFile(DATABASE_PATH)).hits;
-  hits += 1;
-  writeFile(DATABASE_PATH, JSON.stringify({hits}));
-
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number {hits}.</p>
+      <p>You are visitor number <HitCounter />.</p>
     </main>
   );
 }
---
+++ src/components/HitCounter/HitCounter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {
+  readFile,
+  writeFile,
+} from '../../helpers/file-helpers'
+
+const DATABASE_PATH = '/src/database.json';
+
+/*
+`readFile` takes 1 argument:
+• the path to the file:
+
+readFile('/path/to/file');
+
+`writeFile` takes 2 arguments:
+• The path to the file
+• The new contents for the file
+
+writeFile(
+  '/path/to/file',
+  '{ "hello": "world" }'
+);
+*/
+
+function HitCounter() {
+  let hits = JSON.parse(readFile(DATABASE_PATH)).hits;
+  hits += 1;
+  writeFile(DATABASE_PATH, JSON.stringify({hits}));
+
+  return (
+    <button className='censored'>{hits}</button>
+  );
+}
+
+export default HitCounter;
